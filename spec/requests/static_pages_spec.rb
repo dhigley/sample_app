94e5be82b6144eda435f19eb660bcb1d56255842
Daniel Higley <daniel.higley@gmail.com>
2014-08-15 10:15:00 -0400

    Added a user signup page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,6 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-  # Exercise 3: Eliminate some repetition.
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-
   # Eliminate duplication by telling RSpec that 'page' is the subject of the test.
   subject { page }
 
@@ -18,7 +15,8 @@ describe "StaticPages" do
     it { should have_content('Sample App') }
 
     # Listing 4.4: Updated test for the Home page's title.
-    it { should have_title(base_title) }
+    # Listing 5.30: Title is now being tested by spec/support/utilities.
+    it { should have_title(full_title('')) }
 
     # Listing 3.19: title test
     # Listing 4.4: Updated test for the Home page's title.
@@ -33,7 +31,7 @@ describe "StaticPages" do
     it { should have_content('Help') }
 
     # Listing 3.19: title test
-    it { should have_title("#{base_title} | Help") }
+    it { should have_title(full_title('Help')) }
   end
 
   # Listing 3.14: Adding code to test the contents of the About page.
@@ -43,7 +41,7 @@ describe "StaticPages" do
     it { should have_content('About Us') }
 
     # Listing 3.19: title test
-    it { should have_title("#{base_title} | About") }
+    it { should have_title(full_title('About')) }
   end
 
   # Excercises 3: Make a contact page
@@ -51,6 +49,6 @@ describe "StaticPages" do
     before { visit contact_path }
 
     it { should have_content('Contact') }
-    it { should have_title("#{base_title} | Contact") }
+    it { should have_title(full_title('Contact')) }
   end
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+# Listing 5.29: A file for RSpec utilities with a full_title function.
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
