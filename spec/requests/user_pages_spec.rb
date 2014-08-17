3fe9d92a61d2e5ad150efdaef10ec8bf292cbbef
Daniel Higley <daniel.higley@gmail.com>
2014-08-17 18:06:22 -0400

    Added 'error' and 'sucess' message test

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,6 +33,17 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      # Listing 7.31: Testing for invalid submission error page and messages.
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        # My own added test for errors; app should stay on the signup page, it should
+        # display the error message, and it should give a <li> list of errors.
+        it { should have_selector('div.alert.alert-error', 'error') }
+        it { should have_selector('li', text: '*') }
+      end
     end
 
     describe "with valid information" do
@@ -45,6 +56,15 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      # Listing 7.32: Test for post-save behavior in the 'create' action.
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
